93b16bea28b627d515e53c0c95346b3932c72a50
ViniciusGomesIT <vinigomes47@gmail.com>
2018-10-16 18:04:15 -0300

    Adicao das configuracoes do swagger para o endpoint /user

--- TestMicroService-2/src/main/java/br/com/vinicius/controller/impl/UserControllerImpl.java
+++ TestMicroService-2/src/main/java/br/com/vinicius/controller/impl/UserControllerImpl.java
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import br.com.vinicius.controller.UserResources;
 import br.com.vinicius.model.User;
 import br.com.vinicius.service.impl.UserServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Controller
+@Api
 public class UserControllerImpl implements UserResources {
 
 	@Autowired
@@ -19,6 +24,13 @@ public class UserControllerImpl implements UserResources {
 
 	@Override
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	@ApiResponses( value = {
+			@ApiResponse(code = 201, message = "Created", response = User.class),
+			@ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Internal Server Error")
+	})
+	@ApiOperation(value = "Recupera os dados dos usuários.")
 	public ResponseEntity<User> getUserInfo() {
 		System.out.println(service.getUserInfo());
 
