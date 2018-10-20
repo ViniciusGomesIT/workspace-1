d509d080a6a34896585aa3b5739fe5449a55a2ed
Vinicius Gomes <vinigomes47@gmail.com>
2018-10-20 18:49:26 -0300

    Delete para rename dos projetos

--- TestMicroService/src/main/java/br/com/vinicius/service/CommonService.java
+++
@@ -1,9 +0,0 @@
-package br.com.vinicius.service;
-
-import br.com.vinicius.model.Cliente;
-import br.com.vinicius.rest.response.DadosClienteResponse;
-
-public interface CommonService {
-
-	DadosClienteResponse parseResponse(Cliente cliente);
-}
--- TestMicroService/src/main/java/br/com/vinicius/service/DadosClienteService.java
+++
@@ -1,8 +0,0 @@
-package br.com.vinicius.service;
-
-import br.com.vinicius.model.Cliente;
-
-public interface DadosClienteService {
-
-	Cliente findClienteData(String document, String authenticated);
-}
